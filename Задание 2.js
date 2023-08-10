e3f03d3f29edfd91514c4f4eadd37587e2ac6edb
maratakoev <126777609+maratakoev@users.noreply.github.com>
2023-08-10 12:42:44 +0300

    Add files via upload

--- Задание 2.js
+++ Задание 2.js
@@ -1,17 +1,19 @@
-let num1 = +prompt ('введите число от 0 до 1000');
-if (num < 1000) 
-  function isPrime (num){
-    for (let i= 2; i < num; i++) {
-      if (num % i === 0) return false;     
-    }
-    return num !== 1;
+function printPrimes(num) {
+  let result
+  if (num > 1000){
+    result = 'слишком большое число';
+  } else if (num === 1 || num === 0) {
+    result = 'число ни простое, ни составное';
+  } else {
+    isPrime(num) ? result = `$ {num} простое число` result = `{num} составное число`;  
   }
-function printPrimes(1000){
-  for (let i=2; i <=1000; i++) {
-    if (isPrime(num)) == 'true')
-  console.log (num + 'простое число');
-  }
+  return result
 }
-  else {
-   console.log ('слишком большое число');
+
+function isPrime(num){
+  for (let i=2; i < num; i++) {
+    if (num % i === 0) return false;
   }
+  return true;
+}
+console.log (printPrimes(1001))
